b7b23e3a9b30e7b356707d454a38c7845fa38d6b
Agmar Torres <agmartorres@gmail.com>
2020-06-17 22:51:34 -0300

    first commit

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface IRequest {
+  title: string
+  value: number
+  type: "income"| "outcome"
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,9 +14,19 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type}:IRequest): Transaction {
+//Repository vai lidar de todos os nosso dados
+
+    const {total} = this.transactionsRepository.getBalance()
+    if( total < value && type =='outcome'){
+      throw new Error('O valor de saida é maior que o que vc possui em caixa')
+    }
+    const transaction = this.transactionsRepository.create({
+      title, value, type
+    })
+    return transaction
   }
+
 }
 
 export default CreateTransactionService;
